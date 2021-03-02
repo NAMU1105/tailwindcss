2fa875d615c47eaacef1ece285944f2c60864855
namu1105 <namunamu1105@gmail.com>
2021-03-02 20:42:04 +0900

    add addProduct page

--- src/pages/addProduct.jsx
+++ src/pages/addProduct.jsx
@@ -10,7 +10,7 @@ const AddProduct = (props) => {
       <ContainerLayout>
         {/* <div className="md:grid md:grid-cols-3 md:gap-6"> */}
         {/* <div className="mt-5 md:mt-0 md:col-span-2"> */}
-        <div className="flex flex-col">
+        <div className="flex flex-col w-2/3">
           <HeadingFirst align="left">ADD PRODUCT</HeadingFirst>
           <article id="upperPart" className="mt-8">
             <form className="w-full" action="#" method="POST">
@@ -209,8 +209,8 @@ const AddProduct = (props) => {
                   <h3 className="font-bold">Meta Data</h3>
                   <span className="block">Fill all information below</span>
 
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-3">
+                  <div className="grid grid-cols-2 grid-rows-2 gap-6">
+                    <div className="col-span-2 sm:col-span-1">
                       <label
                         htmlFor="first_name"
                         className="block text-sm font-medium text-gray-700"
@@ -249,7 +249,7 @@ const AddProduct = (props) => {
                       >
                         Meta Description
                       </label>
-                      <input
+                      <textarea
                         type="text"
                         name="email_address"
                         id="email_address"
@@ -257,95 +257,20 @@ const AddProduct = (props) => {
                         className="focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                       />
                     </div>
-
-                    {/* <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="country"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Country / Region
-                      </label>
-                      <select
-                        id="country"
-                        name="country"
-                        autoComplete="country"
-                        className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      >
-                        <option>United States</option>
-                        <option>Canada</option>
-                        <option>Mexico</option>
-                      </select>
-                    </div> */}
-
-                    {/* <div className="col-span-6">
-                      <label
-                        htmlFor="street_address"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Street address
-                      </label>
-                      <input
-                        type="text"
-                        name="street_address"
-                        id="street_address"
-                        autoComplete="street-address"
-                        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div> */}
-
-                    {/* <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                      <label
-                        htmlFor="city"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        City
-                      </label>
-                      <input
-                        type="text"
-                        name="city"
-                        id="city"
-                        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div> */}
-
-                    {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                      <label
-                        htmlFor="state"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        State / Province
-                      </label>
-                      <input
-                        type="text"
-                        name="state"
-                        id="state"
-                        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div> */}
-
-                    {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                      <label
-                        htmlFor="postal_code"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        ZIP / Postal
-                      </label>
-                      <input
-                        type="text"
-                        name="postal_code"
-                        id="postal_code"
-                        autoComplete="postal-code"
-                        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div> */}
                   </div>
                 </div>
                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                   <button
                     type="submit"
-                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="mr-5 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
                     Save
+                  </button>
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Cancel
                   </button>
                 </div>
               </div>
